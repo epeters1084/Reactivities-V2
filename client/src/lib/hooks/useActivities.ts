59cb20b75d27d2123c8a6eb4236fc37beef498aa
Ellen Peters <epeters@libertyglobal.com>
2025-12-12 15:40:40 +0000

    End of Section 7

--- client/src/lib/hooks/useActivities.ts
+++ client/src/lib/hooks/useActivities.ts
@@ -1,7 +1,7 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import agent from "../api/agent";
 
-export const useActivities = () => {
+export const useActivities = (id?: string) => {
       const queryClient = useQueryClient();
 
       const { data: activities, isPending } = useQuery({
@@ -9,8 +9,18 @@ export const useActivities = () => {
     queryFn: async () => {
       const response = await agent.get<Activity[]>('/activities')
       return response.data;
-    }
+    },
+
   });
+
+  const { data: activity, isLoading: isLoadingActivity} = useQuery({
+    queryKey: ['activities', id],
+    queryFn: async () => {
+        const response = await agent.get<Activity>(`/activities/${id}`)
+        return response.data
+    },
+        enabled: !!id
+  })
 
   const updateActivity = useMutation({
     mutationFn: async (activity: Activity) => {
@@ -26,7 +36,8 @@ export const useActivities = () => {
 
     const createActivity = useMutation({
     mutationFn: async (activity: Activity) => {
-       await agent.post('/activities', activity) 
+        const response = await agent.post('/activities', activity) ;
+        return response.data;
     },
     onSuccess: async () => {
         await queryClient.invalidateQueries({
@@ -53,6 +64,8 @@ export const useActivities = () => {
     isPending,
     updateActivity,
     createActivity,
-    deleteActivity
+    deleteActivity,
+    activity,
+    isLoadingActivity
   }
 }
